cb15f35517460e07b82cba618c097b1b6fd8eb62
rommel palma <85199190+Rommelpalma123@users.noreply.github.com>
2023-02-03 09:15:11 -0500

    arreglos

--- src/components/Barra.js
+++ src/components/Barra.js
@@ -5,15 +5,15 @@ const  Barra = () =>
 {
     return(
         <div>     
-          <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+          <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <div className="container-fluid">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
               data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
               aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
                 <li className="nav-item">
                     <Link className="nav-link text-light fs-5" aria-current="page" to="/">Inicio</Link>
                   </li>
